f2eecc95d6c2905e35510ff4dc5f6de6d1ef2155
Cari Cesarotti <Cari@dhcp-10-250-185-37.harvard.edu>
2019-12-13 17:55:12 -0500

    added emdCalcReadin.py

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 The github Repository for the Event Isotropy observable (arXiv number)
+Created December 2019 by Cari Cesarotti (ccesarotti@g.harvard.edu)
+
+To run this code, one neads the Python Optimal Transport Library (https://pot.readthedocs.io/en/stable/) and HEALPix (https://astropy-healpix.readthedocs.io/en/latest/) 
 
 emdVar.py
 
 This file includes the calculation of distance between data sets for several ground metrics, as well as the computation of the actual EMD. 
+
+
 
---
+++ eventIsotropy/emdCalcReadin.py
@@ -0,0 +1,65 @@
+######################
+## This file allows users to upload lists of 4-momenta and calculate its EMD from spherical geometry
+## Event must be formatted as <event> .... </event>
+######################
+import sys
+import time
+import warnings
+import numpy as np
+import matplotlib.pylab as plt
+
+from spherGen import sphericalGen, engFromVec
+from emdVar import _cdist_cos, emd_Calc
+
+from matplotlib import rc
+from mpl_toolkits.mplot3d import Axes3D
+rc('text', usetex=True)
+from prettytable import PrettyTable
+
+############################################
+## Specify input file
+if len(sys.argv)<2:
+    print 'Error: user did not specify input file'
+    sys.exit(2)
+
+## Generate spherical sample
+sphereSample = np.array([sphericalGen(i) for i in range(5)])
+sphereEng = np.array([engFromVec(sphereSample[j]) for j in range(5)])
+
+## Choose sphere n points
+spherePoints1 = sphereSample[2]
+sphereEng1 = sphereEng[2]
+
+fileName=sys.argv[1]
+
+momenta=[]
+engL=[]
+file = open(fileName, 'r') 
+if file.closed:
+    print('Error: could not open file')
+    sys.exit(2)
+nextline=file.readline()
+while nextline[0:7]=="<event>":
+    nextline=file.readline()
+    while nextline[0:8]!="</event>":
+        particle = [float(n) for n in nextline.split()]
+        if len(particle)>2:
+            eng, px, py, pz = particle[0], particle[1], particle[2], particle[3]   
+            if eng > 1e-05:
+                momenta.append(np.array([px, py, pz]))
+                engL.append(eng)
+        nextline = file.readline()
+    nextline=file.readline()
+        
+file.close()
+
+
+## Calculate the \semd values 
+
+
+M = _cdist_cos(spherePoints1,np.array(momenta))
+emdval = emd_Calc(sphereEng1, np.array(engL), M)
+print(emdval)
+
+
+
